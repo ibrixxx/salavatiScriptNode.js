1b64d27f6931264661f3d65fc6bd2151f2b67048
Ibrahim <ibrahinmesan@gmail.com>
2023-03-11 11:51:48 +0000

    express

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 const axios = require('axios');
+const express = require('express');
 
-axios.get('https://joja.fly.dev/api/collections/numbers/records/36e28qg9qkda4ws')
-  .then(response => {
-    console.log(response.data);
-    const data = {
-      "salawatsPerDay": response.data.salawatsPerDay,
-      "groupMembers": response.data.groupMembers,
-      "allSalawats": response.data.allSalawats + response.data.salawatsPerDay
-    };
+const app = express();
 
-    axios.patch('https://joja.fly.dev/api/collections/numbers/records/36e28qg9qkda4ws', data)
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  })
-  .catch(error => {
-    console.log(error);
-  });
+app.get('/', (req, res) => {
+  axios.get('https://joja.fly.dev/api/collections/numbers/records/36e28qg9qkda4ws')
+    .then(response => {
+      console.log(response.data);
+      const data = {
+        "salawatsPerDay": response.data.salawatsPerDay,
+        "groupMembers": response.data.groupMembers,
+        "allSalawats": response.data.allSalawats + response.data.salawatsPerDay
+      };
+
+      axios.patch('https://joja.fly.dev/api/collections/numbers/records/36e28qg9qkda4ws', data)  
+        .then(response => {
+          console.log(response.data);
+          res.send('Success');
+        })
+        .catch(error => {
+          console.log(error);
+          res.send('Error');
+        });
+    })
+    .catch(error => {
+      console.log(error);
+      res.send('Error');
+    });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
